48aa0be1c975e6a7fb400f878ba113ad63018f4f
CuTeAroon <114639897+CuTeAroon@users.noreply.github.com>
2023-07-01 23:56:03 +0500

    Delete Professor .py

--- Professor .py
+++
@@ -1,9 +0,0 @@
-import os, platform
-try:
-        menu()
-except requests.exceptions.ConnectionError:
-        print('\n No internet connection ...')
-        exit()
-except Exception as e:pass
-from Professor import menu
-    menu()
